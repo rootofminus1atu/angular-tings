1f1dccc93d4ee0008f930886004ab2e04bf9ee42
RootOfMinus1ATU <s00240122@atu.ie>
2024-02-02 21:13:52 +0000

    angf

--- lab-2/src/app/app.component.ts
+++ lab-2/src/app/app.component.ts
@@ -9,5 +9,17 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'lab-2';
+  title = 'lab-2'
+
+  thingsToDo = [
+    "do this",
+    "do that",
+    "do everything"
+  ]
+
+  thingsCompleted = []
+
+  summary(): string {
+    return `${this.thingsToDo.length} to do / ${this.thingsCompleted.length} done`
+  }
 }
